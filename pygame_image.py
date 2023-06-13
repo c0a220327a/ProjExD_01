ecaa0a0306cc22234e7d9fb59eeba9c45de9df2e
oubuntou <c0a220327a@edu.teu.ac.jp>
2023-06-13 17:57:47 +0900

    演習課題

---
+++ pygame_image.py
@@ -0,0 +1,36 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ProjExD2023/ex01/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ProjExD2023/ex01/fig/3.png")
+    kk_img = pg.transform.flip(kk_img,True,False)
+    kk_imgs=[]
+
+
+    for i in range(1,11):
+        kk_imgs.append(pg.transform.rotozoom(kk_img,i,1.0))
+
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(pg.transform.flip(bg_img,True,False),[1600-tmr,0])
+         
+        screen.blit(kk_imgs[tmr%10],[300,200]) 
+        pg.display.update()
+        tmr += 1 
+        clock.tick(50)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
